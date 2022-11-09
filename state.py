870d1b0a6abbb93f85f703b9c09e81a68f3658b9
aaserfawzy <aaserkind537@gmail.com>
2022-11-09 15:47:02 +0200

    updatedState

--- state.py
+++ state.py
@@ -3,10 +3,14 @@ class state:
     remain=0
     k=0
     depth=0
-    def __init__(self, board, remain,k,depth):
+    Aiscore=0
+    humanscroe=0
+    def __init__(self, board, remain,k,depth,Aiscore,humanscroe):
         self.board=board
         self.remain=remain
         self.k=k
         self.depth=depth
-        
+        self.Aiscore=Aiscore
+        self.humanscroe=humanscroe
+
        
--- utilities.py
+++ utilities.py
@@ -20,4 +20,17 @@ class utilities:
                 next=temp2 & ( pos <<  (9*i) )
                 actions.append(state(next,remain,k,depth))
         return actions
-        
+
+    def heuristic(sta):
+        Aiscore=0.0
+        humanscore=0.0
+        # check points for computer
+        for i in range(7):
+            temp=sta.board
+            temp2=sta.board
+            pos=(temp>>(9*i)) & 7
+            
+        # check points for player
+
+        # check potential score for computer
+        # check potential score for player

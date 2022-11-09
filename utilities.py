b88afd4557ac0053442cd7bd248d9170546ae7d8
aaserfawzy <aaserkind537@gmail.com>
2022-11-09 14:54:30 +0200

    untestedActions

--- utilities.py
+++ utilities.py
@@ -1,4 +1,23 @@
+from state import state
 class utilities:
     
-    def action(board,type):
-        return None
+    def action(sta,type):
+        bit=1
+        if type=="h":
+            bit=0
+        actions=[]
+        remain=sta.remain-1
+        k=sta.k
+        depth=sta.dep+1
+        for i in range(7):
+            temp=sta.board
+            temp2=sta.board
+            pos=(temp>>(9*i)) & 7
+            if (pos)<7:
+                next=temp2 |(bit << ( (9*i)+(3+pos) ) )
+                pos+=1
+                next=temp2 |( 7 <<  (9*i) )
+                next=temp2 & ( pos <<  (9*i) )
+                actions.append(state(next,remain,k,depth))
+        return actions
+        

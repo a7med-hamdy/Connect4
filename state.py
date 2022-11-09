ce2f3ffcddce7eb894036ed2090262a0d9b6f694
aaserfawzy <aaserkind537@gmail.com>
2022-11-09 13:46:30 +0200

    state

---
+++ state.py
@@ -0,0 +1,12 @@
+class state:
+    board=0
+    remain=0
+    k=0
+    depth=0
+    def __init__(self, board, remain,k,depth):
+        self.board=board
+        self.remain=remain
+        self.k=k
+        self.depth=depth
+        
+       
